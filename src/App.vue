5bc6953b566b8efcaebe40fecfbf47eafa63156c
georgia <georgiab543@gmail.com>
2024-02-14 00:41:22 -0300

    feat: ajustando app

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import Form from "./components/Form.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    BarraLateral,
+    Form
+  }
+});
+</script>
+
+<template>
+  <main class="columns is-gapless is-multiline is-flex">
+    <div class="column is-one-quarter is-fullheight">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+    </div>
+      <Form />
+  </main>
+</template>
+
+
+<style scoped>
+
+</style>
