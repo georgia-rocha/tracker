86ea3b805b1336842c66104e63eb47223248d872
georgia <georgiab543@gmail.com>
2024-02-14 22:36:20 -0300

    feat: cria form com preparação para componentizar cronometro

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,7 +2,7 @@
   <div class="box">
     <div class="columns">
       <div
-        class="column is-8"
+        class="column is-8 is-full" is-full aqui 
         role="form"
         aria-label="Formulário para criação de uma nova tarefa"
       >
@@ -15,16 +15,15 @@
           <div
             class="is-flex is-align-items-center is-justify-content-space-between"
           >
-            <section>
-              <strong>00:00:00</strong>
-            </section>
-            <button class="button">
+           <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
+
+            <button class="button" @click="iniciar">
               <span class="icon">
                 <i class="fas fa-play"></i>
               </span>
               <span>play</span>
             </button>
-            <button class="button">
+            <button class="button" @click="finalizar">
               <span class="icon">
                 <i class="fas fa-stop"></i>
               </span>
@@ -38,11 +37,45 @@
 </template>
 
 <script lang="ts">
+
 import { defineComponent } from "vue";
+import Cronometro from './Cronometro.vue';
 
 export default defineComponent({
   name: "Form",
+  components: {
+    Cronometro
+  },
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      intervalId: null,
+    };
+  },
+ 
+  methods: {
+    iniciar() {
+      this.finalizar();
+      this.intervalId = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+      console.log("Iniciando");
+    },
+
+    finalizar() {
+      if (this.intervalId !== null) {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+        console.log("Finalizando");
+      }
+    },
+  },
+
+  beforeDestroy() {
+    this.finalizar();
+  },
 });
+
 </script>
 
 <style scoped>
