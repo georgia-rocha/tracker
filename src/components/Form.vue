e3bf171d48ac6958dd3799324aa2134bab19d4c0
georgia <georgiab543@gmail.com>
2024-02-14 22:46:08 -0300

    feat: componentizando temporizador

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,7 +2,9 @@
   <div class="box">
     <div class="columns">
       <div
-        class="column is-8 is-full" is-full aqui 
+        class="column is-8 is-full"
+        is-full
+        aqui
         role="form"
         aria-label="Formulário para criação de uma nova tarefa"
       >
@@ -12,24 +14,7 @@
           placeholder="Qual tarefa você deseja iniciar?"
         />
         <div class="column">
-          <div
-            class="is-flex is-align-items-center is-justify-content-space-between"
-          >
-           <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
-
-            <button class="button" @click="iniciar">
-              <span class="icon">
-                <i class="fas fa-play"></i>
-              </span>
-              <span>play</span>
-            </button>
-            <button class="button" @click="finalizar">
-              <span class="icon">
-                <i class="fas fa-stop"></i>
-              </span>
-              <span>stop</span>
-            </button>
-          </div>
+         <Temporizador />
         </div>
       </div>
     </div>
@@ -37,45 +22,16 @@
 </template>
 
 <script lang="ts">
-
 import { defineComponent } from "vue";
-import Cronometro from './Cronometro.vue';
+import Temporizador from "./Temporizador.vue";
 
 export default defineComponent({
   name: "Form",
   components: {
-    Cronometro
+    Temporizador
   },
-  data() {
-    return {
-      tempoEmSegundos: 0,
-      intervalId: null,
-    };
-  },
- 
-  methods: {
-    iniciar() {
-      this.finalizar();
-      this.intervalId = setInterval(() => {
-        this.tempoEmSegundos += 1;
-      }, 1000);
-      console.log("Iniciando");
-    },
-
-    finalizar() {
-      if (this.intervalId !== null) {
-        clearInterval(this.intervalId);
-        this.intervalId = null;
-        console.log("Finalizando");
-      }
-    },
-  },
-
-  beforeDestroy() {
-    this.finalizar();
-  },
+  
 });
-
 </script>
 
 <style scoped>
---
+++ src/components/Temporizador.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+    <button class="button" @click="iniciar">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalizar">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue";
+
+export default defineComponent({
+  name: "Temporizador",
+  components: {
+    Cronometro
+  },
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      intervalId: null,
+    };
+  },
+
+  methods: {
+    iniciar() {
+      this.finalizar();
+      this.intervalId = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+      console.log("Iniciando");
+    },
+
+    finalizar() {
+      if (this.intervalId !== null) {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+        console.log("Finalizando");
+      }
+    },
+  },
+
+  beforeDestroy() {
+    this.finalizar();
+  },
+});
+</script>
+
+<style scoped>
+</style>
