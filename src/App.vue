e9e024c01a4e462424d377d9aedfe76ec7c92243
georgia <georgiab543@gmail.com>
2024-02-21 20:18:44 -0300

    feat: preparando App para salvar as tarefas

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,42 @@
 import { defineComponent } from "vue";
 import BarraLateral from "./components/BarraLateral.vue";
 import Form from "./components/Form.vue";
+import Tarefa from "./components/Tarefa.vue";
+import ITarefa from "./interfaces/ITarefa"
 
 export default defineComponent({
   name: "App",
   components: {
     BarraLateral,
-    Form
+    Form,
+    Tarefa
+  },
+  data () {
+    return {
+      tarefas: [] as ITarefa[]
+    }
   }
 });
 </script>
 
 <template>
-  <main class="columns is-gapless is-multiline is-flex">
-    <div class="column is-one-quarter is-fullheight">
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
       <BarraLateral />
     </div>
     <div class="column is-three-quarter">
       <Form />
+      <div class="lista">
+        <Tarefa />
+      </div>
     </div>
   </main>
 </template>
 
 
 <style scoped>
+.lista {
+  padding: 0.5rem;
 
+}
 </style>
