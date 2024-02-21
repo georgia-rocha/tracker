d2a20d0e7304f1a982b489a3f4cfdcc1f3ee83c2
georgia <georgiab543@gmail.com>
2024-02-21 20:19:28 -0300

    feat: criando componente tarefa

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="box">
+    <div class="tarefa">
+      <h1 class="">Descrição da Tarefa</h1>
+      <Cronometro :tempoEmSegundos="15" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue";
+
+export default defineComponent({
+  name: "Tarefa",
+  components: {
+    Cronometro,
+  },
+});
+</script>
+
+<style scoped>
+.box {
+  background: #faf0ca;
+}
+.tarefa {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
