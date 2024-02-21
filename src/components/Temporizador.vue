260d2ad56d7a85e1de805860f76b208a86d35ca5
georgia <georgiab543@gmail.com>
2024-02-21 20:18:13 -0300

    fix: ajustando Temporizador

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -2,15 +2,9 @@
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <Cronometro :tempoEmSegundos="tempoEmSegundos" />
     <button class="button" @click="iniciar" :disabled="cronometroAtivo">
-      <span class="icon">
-        <i class="fas fa-play"></i>
-      </span>
       <span>play</span>
     </button>
     <button class="button" @click="finalizar" :disabled="!cronometroAtivo">
-      <span class="icon">
-        <i class="fas fa-stop"></i>
-      </span>
       <span>stop</span>
     </button>
   </div>
